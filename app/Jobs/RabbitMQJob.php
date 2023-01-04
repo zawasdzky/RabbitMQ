c60e534bd7756cf0375762a27247a402e392aa02
Jean Pierre <zawasdzky@gmail.com>
2023-01-04 11:26:44 -0500

    job rabbit

---
+++ app/Jobs/RabbitMQJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\ConsumerCouponController;
+use Carbon\Carbon;
+
+
+class RabbitMQJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function handle()
+    {
+        try {
+           Log::info($this->data);
+        }
+        catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+    }
+}
